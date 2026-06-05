9a934d6bba44734117a5a07507abe80cb39ddd75
accorrales <andercorrales28@gmail.com>
2026-06-04 20:10:29 -0600

    cambia email service a resend

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,4 +1,6 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
