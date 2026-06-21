dbdaf2a8643472daaf5aa7f481dd8d1b7e497d86
accorrales <andercorrales28@gmail.com>
2026-06-21 17:31:23 -0600

    arreglo 1.3

--- src/routes/comprasEntradasRoutes.js
+++ src/routes/comprasEntradasRoutes.js
@@ -6,7 +6,7 @@ const comprasEntradasController = require('../controllers/comprasEntradasControl
 router.post('/', comprasEntradasController.crearCompra);
 router.get('/pendientes', comprasEntradasController.listarPendientes);
 router.get('/qr/:uuid', comprasEntradasController.obtenerQrEntrada);
-//router.get('/wallet/apple/:uuid', comprasEntradasController.obtenerAppleWallet);
+router.get('/wallet/apple/:uuid', comprasEntradasController.obtenerAppleWallet);
 router.get('/wallet/google/:uuid', comprasEntradasController.obtenerGoogleWallet);
 router.get('/test/email', comprasEntradasController.testEmail);
 router.get('/:id', comprasEntradasController.obtenerCompraPorId);
